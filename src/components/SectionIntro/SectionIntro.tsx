cca39a2e2e61aeffa3b6455ae59cb5a9936f6bd9
YerikAH <yerik05vh@gmail.com>
2023-07-07 01:05:37 -0500

    :lipstick: Update styles for the component CardIntro

--- src/components/SectionIntro/SectionIntro.tsx
+++ src/components/SectionIntro/SectionIntro.tsx
@@ -1,19 +1,51 @@
-import Carousel from '../Carousel/Carousel'
-import CardIntro from '../CardIntro/CardIntro'
-import {SectionIntroProps} from '../../interface/props'
-import s from './SectionIntro.module.css'
+import Carousel from "../Carousel/Carousel";
+import CardIntro from "../CardIntro/CardIntro";
+import { SectionIntroProps } from "../../interface/props";
+import s from "./SectionIntro.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-function SectionIntro({images, speed, items}: SectionIntroProps){
+function SectionIntro({ images, speed, items }: SectionIntroProps) {
   return (
-    <section>
-      <Carousel images={images} speed={speed}/>
-      <div className={s.cards}>
-        {items.map((item, idx) => (
-          <CardIntro key={idx} button={item.button} image={item.image} text={item.text} title={item.title}/>
-        ))}
+    <section className={s.section}>
+      <Carousel images={images} speed={speed} />
+      <div className={s.container}>
+        <div className={s.cards}>
+          {items.map((item, idx) => (
+            <CardIntro
+              key={idx}
+              button={item.button}
+              image={item.image}
+              text={item.text}
+              title={item.title}
+            />
+          ))}
+        </div>
+
+        <Swiper
+          effect="slide"
+          grabCursor={true}
+          loop={true}
+          zoom={true}
+          slidesPerView={1}
+          scrollbar={{ draggable: true }}
+          id={`${s["cards--mobile"]}`}
+        >
+          {items.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <CardIntro
+                key={idx}
+                button={item.button}
+                image={item.image}
+                text={item.text}
+                title={item.title}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
-  )
+  );
 }
 
-export default SectionIntro
+export default SectionIntro;
