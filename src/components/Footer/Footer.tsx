ccc44149c7b4d7e430ebc536a663489d28d62b7f
YerikAH <yerik05vh@gmail.com>
2023-07-12 23:22:23 -0500

    :lipstick: Update styles the footer component.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import s from "./Footer.module.css";
-import {FooterProps} from '../../interface/props'
+import { FooterProps } from "../../interface/props";
 import FooterQuestion from "../FooterQuestion/FooterQuestion";
 
 function Footer({
@@ -12,39 +12,45 @@ function Footer({
   labelContact,
   copyright,
   question,
-  button
+  button,
 }: FooterProps) {
   return (
     <footer className={s.footer}>
-      <FooterQuestion text={question} button={button}/>
+      <FooterQuestion text={question} button={button} />
       <div className={s.footer__contain}>
         <div className={s.footer__contain__info}>
-          <img src={logo} className={s.footer__contain__info__logo} alt="logo" />
+          <img
+            src={logo}
+            className={s.footer__contain__info__logo}
+            alt="logo"
+          />
           <p className={s.text}>{text}</p>
           <div>
             {media.map((item, idx) => (
               <a key={idx} href={item.url} tabIndex={1}>
-                <img src={item.media}  alt="social media" />
+                <img src={item.media} alt="social media" />
               </a>
             ))}
           </div>
         </div>
         <div className={s.footer__contain__navigation}>
-          <h2 className={`${s['text--bold']} ${s.text}`} >{labelNavigation}</h2>
+          <h2 className={`${s["text--bold"]} ${s.text}`}>{labelNavigation}</h2>
           <ul>
             {navigation.map((item, idx) => (
               <li key={idx}>
-                <a href={item.path} className={s.text} tabIndex={1}>{item.label}</a>
+                <a href={item.path} className={s.text} tabIndex={1}>
+                  {item.label}
+                </a>
               </li>
             ))}
           </ul>
         </div>
         <div className={s.footer__contain__contact}>
-          <h2 className={`${s['text--bold']} ${s.text}`}>{labelContact}</h2>
+          <h2 className={`${s["text--bold"]} ${s.text}`}>{labelContact}</h2>
           <ul>
             {contacts.map((item, idx) => (
-              <li key={idx} >
-                <img src={item.icon} alt="icon"/>
+              <li key={idx}>
+                <img src={item.icon} alt="icon" />
                 <p className={s.text}>{item.label}</p>
               </li>
             ))}
