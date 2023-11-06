66a6b10a014a1417765e66c4593961f401b8e595
Harvey Yerik <yerik05vh@gmail.com>
2023-11-06 10:17:33 -0500

    :bug: Fix the error of FormContant component.

--- src/components/FormContact/FormContact.tsx
+++ src/components/FormContact/FormContact.tsx
@@ -6,32 +6,17 @@ function FormContact({ inputs }: FormContactProps) {
   return (
     <form className={s.form}>
       {inputs.map((item, idx) => (
-        <>
-          {idx > 1 ? (
-            <div className={s.input__full} key={idx}>
-              <CustomInput
-                name={item.name}
-                label={item.label}
-                placeholder={item.placeholder}
-                withIcon={item.withIcon}
-                multiline={item.multiline}
-                type={item.type}
-                isRequired={item.isRequired}
-              />
-            </div>
-          ) : (
-            <CustomInput
-              key={idx}
-              name={item.name}
-              label={item.label}
-              placeholder={item.placeholder}
-              withIcon={item.withIcon}
-              type={item.type}
-              multiline={item.multiline}
-              isRequired={item.isRequired}
-            />
-          )}
-        </>
+        <div key={idx} className={idx > 1 ? `${s.input__full}` : ""}>
+          <CustomInput
+            name={item.name}
+            label={item.label}
+            placeholder={item.placeholder}
+            withIcon={item.withIcon}
+            multiline={item.multiline}
+            type={item.type}
+            isRequired={item.isRequired}
+          />
+        </div>
       ))}
 
       <input type="submit" value="Manda mensaje" name="submit" />
