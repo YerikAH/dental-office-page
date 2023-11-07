7a9ba53057746ce88bd006dd84d3fa52f416e2bf
Harvey Yerik <yerik05vh@gmail.com>
2023-11-07 10:49:11 -0500

    :iphone: Work on responsive design

--- src/components/ContactSupport/ContactSupport.tsx
+++ src/components/ContactSupport/ContactSupport.tsx
@@ -1,38 +1,39 @@
 import s from "./ContactSupport.module.css";
 import { ContactSupportProps } from "../../interface/props";
+import headphones from "../../assets/headphones-icon.png";
+import settings from "../../assets/setting-icon.png";
+import curve from "../../assets/curve.svg";
 
-function ContactSupport({ title, text, email, phoneNumber }: ContactSupportProps) {
+function ContactSupport({
+  title,
+  text,
+  email,
+  phoneNumber,
+}: ContactSupportProps) {
   return (
     <section className={s.section}>
       <h2>{title}</h2>
       <p>{text}</p>
       <div className={s.section__package}>
-        <div>
-          <img src="" alt="image" />
-          <p>Customer service support</p>
+        <div className={s.section__package__icon}>
+          <img src={headphones} alt="image" />
+          <p>customer service support</p>
         </div>
-        <div>
-          <img src="" alt="image" />
-          <p>Technical support</p>
+        <div className={s.section__package__icon}>
+          <img src={settings} alt="image" />
+          <p>technical support</p>
         </div>
-        <div>
+        <div className={s.section__package__contact}>
           <p>please contact us at</p>
-          <span>{email}</span>
+          <span>{phoneNumber}</span>
         </div>
       </div>
-      <div className={s.section__package}>
-        <div>
-          <img src="" alt="image" />
-          <p>Customer service support</p>
-        </div>
-        <div>
-          <img src="" alt="image" />
-          <p>Technical support</p>
-        </div>
-        <div>
-          <p>please contact us at</p>
-          <span>{email}</span>
-        </div>
+      <div className={`${s.section__package} ${s.section__package__special}`}>
+        <p>
+          For questions about hiring or to chedule a La preventiva client demo,{" "}
+          <br /> please contact contact us at <span>{email}</span>
+        </p>
+        <img className={s.curve} src={curve} />
       </div>
     </section>
   );
